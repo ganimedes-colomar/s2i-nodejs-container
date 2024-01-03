575298e9edb36d92d0913e50e7ecc63f370cb3c5
ganimedes-colomar <46420621+ganimedes-colomar@users.noreply.github.com>
2024-01-03 12:09:26 +0100

    Revert port change

--- test/test-app/server.js
+++ test/test-app/server.js
@@ -1,7 +1,7 @@
 console.log("Server started");
 var Msg = '';
 var WebSocketServer = require('ws').Server
-   , wss = new WebSocketServer({port: 443});
+   , wss = new WebSocketServer({port: 8010});
    wss.on('connection', function(ws) {
       ws.on('message', function(message) {
          console.log('Received from client: %s', message);
